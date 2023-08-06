403ba7eff8d4ce12687d063b4a949bf75e493478
Konrad Stepniak <vesphy@gmail.com>
2023-08-06 21:23:54 +0200

    Parallelize getting the forecast

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,46 @@
-use weather_scrapper::{windguru, db};
+use std::sync::Arc;
 
+use tokio::sync::Mutex;
+use weather_scrapper::{db, windguru};
 
 #[tokio::main]
-async fn main() -> Result<(), tokio_postgres::Error>{
-    let windguru = windguru::Client::new();
-    let db = db::Client::new().await?;
+async fn main() -> Result<(), tokio_postgres::Error> {
+    let db = Arc::new(Mutex::new(db::Client::new().await?));
+    let spots = db.lock().await.get_all_spots().await?;
 
-    for spot in &db.get_all_spots().await? {
-        let forecast = windguru.get_forecast(spot.windguru_spot_id)
-            .await
-            .expect("Windguru should return forecast for the spot");
+    let mut futures = vec![];
 
-        for entry in &forecast.entries {
-            db.upsert_forecast(spot.spot_id, entry).await?;
-        }
+    for spot in &spots {
+        let db = Arc::clone(&db);
+
+        let windguru_spot_id = spot.windguru_spot_id;
+        let spot_id = spot.spot_id;
+        let name = spot.name.clone();
+
+        futures.push(tokio::spawn(async move {
+            let windguru = windguru::Client::new();
+
+            let forecast = windguru
+                .get_forecast(windguru_spot_id)
+                .await
+                .expect("Windguru should return forecast for the spot");
+            println!("Downloaded forecast for {spot_id}: {name}");
+
+            for entry in &forecast.entries {
+                db.lock()
+                    .await
+                    .upsert_forecast(spot_id, entry)
+                    .await
+                    .expect("should upsert properly in the db")
+            }
+
+            println!("Upserted forecast for {spot_id}: {name}");
+        }))
     }
-    
+
+    for future in futures {
+        let _ = future.await;
+    }
+
     Ok(())
 }
